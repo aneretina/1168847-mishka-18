061c32419a39716e77354c1bf9a61d325248fa9f
Anna Neretinaя <neretinaaa@gmail.com>
2020-01-10 13:39:33 +0100

    modal

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 var popup = document.querySelector(".modal__login");
 var link = document.querySelector(".hit__order-btn");
 var shadow = document.querySelector(".modal__shadow");
+var catalogpopup = document.querySelector(".catalog-list__basket");
 
 
 link.addEventListener("click", function(evt) {
@@ -10,6 +11,17 @@ link.addEventListener("click", function(evt) {
  user.focus();
 });
 
+catalogpopup.addEventListener("click", function(evt) {
+ evt.preventDefault();
+ popup.classList.add("modal__show");
+ shadow.classList.add("modal__show");
+ user.focus();
+});
+
 shadow.addEventListener("click", function (evt) {
   popup.classList.remove("modal__show");
 });
+
+
+var nav = document.querySelector(".main-navigation");
+var open = document.querySelector(".main-navigation__toggle");
