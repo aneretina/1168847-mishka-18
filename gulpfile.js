ff71e4be7a0a8b5e7e3626adfbd0d9d2a274e195
Anna Neretinaя <neretinaaa@gmail.com>
2020-01-26 13:36:52 +0100

    1

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ gulp.task("server", function () {
   server.init({
     server: "build/"
   });
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/less/**/*.less", gulp.series("css", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "refresh"));
   gulp.watch("source/*.html", gulp.series("copy", "refresh"));
  });
